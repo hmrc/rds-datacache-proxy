f33887b8fa393cbfc175971b9526e4acd8952132
javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2026-04-21 15:56:31 +0100

    DTR-4095 updated password and clean get registration - work with local oracle

--- app/uk/gov/hmrc/rdsdatacacheproxy/gambling/repositories/GamblingDataCacheRepository.scala
+++ app/uk/gov/hmrc/rdsdatacacheproxy/gambling/repositories/GamblingDataCacheRepository.scala
@@ -18,10 +18,10 @@ package uk.gov.hmrc.rdsdatacacheproxy.gambling.repositories
 
 import play.api.Logging
 import play.api.db.{Database, NamedDatabase}
-import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.{GroupMember, MgdCertificate, PartnerMember, ReturnPeriodEndDate, ReturnSummary}
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.*
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, blocking}
 
 trait GamblingDataSource {
   def getReturnSummary(mgdRegNumber: String): Future[ReturnSummary]
@@ -38,156 +38,159 @@ class GamblingDataCacheRepository @Inject() (
   override def getMgdCertificate(mgdRegNumber: String): Future[MgdCertificate] = {
     logger.info(s"getMgdCertificate - MGD Reg Number: $mgdRegNumber")
 
-    Future {
+    Future(blocking {
       db.withConnection { connection =>
 
-        val storedProcedure =
+        val cs =
           connection.prepareCall(
-            "{call MGD_DC_RTN_PCK.GET_MGD_CERTIFICATE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}"
+            "{call MGD_DC_RTN_PCK.GET_MGD_CERTIFICATE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}"
           )
 
-        // Input
-        storedProcedure.setString("P_MGD_REG_NUMBER", mgdRegNumber)
-
-        // Scalars
-        storedProcedure.registerOutParameter("MGD_REG_NUMBER", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REGISTRATION_DATE", java.sql.Types.DATE)
-        storedProcedure.registerOutParameter("INDIVIDUAL_NAME", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUSINESS_NAME", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("TRADING_NAME", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_NAME", java.sql.Types.VARCHAR)
-
-        storedProcedure.registerOutParameter("BUS_ADDR_LINE1", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_ADDR_LINE2", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_ADDR_LINE3", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_ADDR_LINE4", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_POSTCODE", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_COUNTRY", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUS_ADI", java.sql.Types.VARCHAR)
-
-        storedProcedure.registerOutParameter("REP_MEM_LINE1", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_LINE2", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_LINE3", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_LINE4", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_POSTCODE", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("REP_MEM_ADI", java.sql.Types.VARCHAR)
-
-        storedProcedure.registerOutParameter("TYPE_OF_BUSINESS", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("BUSINESS_TRADE_CLASS", java.sql.Types.NUMERIC)
-        storedProcedure.registerOutParameter("NO_OF_PARTNERS", java.sql.Types.NUMERIC)
-
-        storedProcedure.registerOutParameter("GROUP_REG", java.sql.Types.VARCHAR)
-        storedProcedure.registerOutParameter("NO_OF_GROUP_MEMS", java.sql.Types.NUMERIC)
-
-        storedProcedure.registerOutParameter("DATE_CERT_ISSUED", java.sql.Types.DATE)
-
-        // CURSORS
-
-        storedProcedure.registerOutParameter("P_PART_MEMBERS", oracle.jdbc.OracleTypes.CURSOR)
-        storedProcedure.registerOutParameter("P_GROUP_MEMBERS", oracle.jdbc.OracleTypes.CURSOR)
-        storedProcedure.registerOutParameter("RETURN_PERIOD_END_DATES", oracle.jdbc.OracleTypes.CURSOR)
-
-        storedProcedure.execute()
-
-        // Helper functions
-
-        def optString(name: String) = Option(storedProcedure.getString(name))
-
-        def optDate(name: String) = Option(storedProcedure.getDate(name)).map(_.toLocalDate)
-
-        def optInt(name: String) = Option(storedProcedure.getObject(name)).map(_.toString.toInt)
-
-        // PART MEMBERS CURSOR
-        val partRs =
-          storedProcedure.getObject("P_PART_MEMBERS", classOf[java.sql.ResultSet])
-
-        val partMembers =
-          Iterator
-            .continually(partRs.next())
-            .takeWhile(identity)
-            .map { _ =>
-              PartnerMember(
-                namesOfPartMems    = partRs.getString("names_of_part_mems"),
-                solePropTitle      = Option(partRs.getString("sole_prop_title")),
-                solePropFirstName  = Option(partRs.getString("sole_prop_first_name")),
-                solePropMiddleName = Option(partRs.getString("sole_prop_middle_name")),
-                solePropLastName   = Option(partRs.getString("sole_prop_last_name")),
-                typeOfBusiness     = partRs.getInt("type_of_business")
-              )
+        // close helper
+        def closeQuietly(c: AutoCloseable): Unit =
+          if (c != null)
+            try c.close()
+            catch {
+              case _: Throwable => ()
             }
-            .toList
 
-        partRs.close()
+        try {
+          cs.setString(1, mgdRegNumber)
 
-        // GROUP MEMBERS CURSOR
-        val groupRs =
-          storedProcedure.getObject("P_GROUP_MEMBERS", classOf[java.sql.ResultSet])
+          cs.registerOutParameter(2, java.sql.Types.VARCHAR) // MGD_REG_NUMBER
+          cs.registerOutParameter(3, java.sql.Types.DATE) // REGISTRATION_DATE
+          cs.registerOutParameter(4, java.sql.Types.VARCHAR) // INDIVIDUAL_NAME
+          cs.registerOutParameter(5, java.sql.Types.VARCHAR) // BUSINESS_NAME
+          cs.registerOutParameter(6, java.sql.Types.VARCHAR) // TRADING_NAME
+          cs.registerOutParameter(7, java.sql.Types.VARCHAR) // REP_MEM_NAME
 
-        val groupMembers =
-          Iterator
-            .continually(groupRs.next())
-            .takeWhile(identity)
-            .map(_ => GroupMember(groupRs.getString("names_of_group_mems")))
-            .toList
+          cs.registerOutParameter(8, java.sql.Types.VARCHAR) // BUS_ADDR_LINE1
+          cs.registerOutParameter(9, java.sql.Types.VARCHAR) // BUS_ADDR_LINE2
+          cs.registerOutParameter(10, java.sql.Types.VARCHAR) // BUS_ADDR_LINE3
+          cs.registerOutParameter(11, java.sql.Types.VARCHAR) // BUS_ADDR_LINE4
+          cs.registerOutParameter(12, java.sql.Types.VARCHAR) // BUS_POSTCODE
+          cs.registerOutParameter(13, java.sql.Types.VARCHAR) // BUS_COUNTRY
+          cs.registerOutParameter(14, java.sql.Types.VARCHAR) // BUS_ADI (VARCHAR2)
 
-        groupRs.close()
+          cs.registerOutParameter(15, java.sql.Types.VARCHAR) // REP_MEM_LINE1
+          cs.registerOutParameter(16, java.sql.Types.VARCHAR) // REP_MEM_LINE2
+          cs.registerOutParameter(17, java.sql.Types.VARCHAR) // REP_MEM_LINE3
+          cs.registerOutParameter(18, java.sql.Types.VARCHAR) // REP_MEM_LINE4
+          cs.registerOutParameter(19, java.sql.Types.VARCHAR) // REP_MEM_POSTCODE
+          cs.registerOutParameter(20, java.sql.Types.VARCHAR) // REP_MEM_ADI (VARCHAR2)
 
-        // RETURN PERIOD CURSOR
+          cs.registerOutParameter(21, java.sql.Types.VARCHAR) // TYPE_OF_BUSINESS (VARCHAR2)
+          cs.registerOutParameter(22, java.sql.Types.NUMERIC) // BUSINESS_TRADE_CLASS (NUMBER)
+          cs.registerOutParameter(23, java.sql.Types.NUMERIC) // NO_OF_PARTNERS (NUMBER)
 
-        val returnRs =
-          storedProcedure.getObject("RETURN_PERIOD_END_DATES", classOf[java.sql.ResultSet])
+          cs.registerOutParameter(24, oracle.jdbc.OracleTypes.CURSOR) // P_PART_MEMBERS (REF CURSOR)
+          cs.registerOutParameter(25, java.sql.Types.VARCHAR) // GROUP_REG (VARCHAR2)
+          cs.registerOutParameter(26, java.sql.Types.NUMERIC) // NO_OF_GROUP_MEMS (NUMBER)
+          cs.registerOutParameter(27, oracle.jdbc.OracleTypes.CURSOR) // P_GROUP_MEMBERS (REF CURSOR)
+          cs.registerOutParameter(28, java.sql.Types.DATE) // DATE_CERT_ISSUED (DATE)
+          cs.registerOutParameter(29, oracle.jdbc.OracleTypes.CURSOR) // RETURN_PERIOD_END_DATES (REF CURSOR)
 
-        val returnPeriods =
-          Iterator
-            .continually(returnRs.next())
-            .takeWhile(identity)
-            .map { _ =>
-              ReturnPeriodEndDate(
-                returnPeriodEndDate = returnRs.getDate("return_period_end_date").toLocalDate
-              )
+          cs.execute()
+
+          def optString(i: Int): Option[String] =
+            Option(cs.getString(i)).map(_.trim).filter(_.nonEmpty)
+
+          def optDate(i: Int): Option[java.time.LocalDate] =
+            Option(cs.getDate(i)).map(_.toLocalDate)
+
+          def optInt(i: Int): Option[Int] =
+            Option(cs.getObject(i)).map {
+              case bd: java.math.BigDecimal => bd.intValue()
+              case n: java.lang.Number      => n.intValue()
+              case other                    => other.toString.toInt
             }
-            .toList
 
-        returnRs.close()
+          val partMembers: List[PartnerMember] = {
+            val rs = cs.getObject(24).asInstanceOf[java.sql.ResultSet]
+            if (rs == null) Nil
+            else {
+              try {
+                val b = List.newBuilder[PartnerMember]
+                while (rs.next()) {
+                  b += PartnerMember(
+                    namesOfPartMems    = rs.getString("names_of_part_mems"),
+                    solePropTitle      = Option(rs.getString("sole_prop_title")),
+                    solePropFirstName  = Option(rs.getString("sole_prop_first_name")),
+                    solePropMiddleName = Option(rs.getString("sole_prop_middle_name")),
+                    solePropLastName   = Option(rs.getString("sole_prop_last_name")),
+                    typeOfBusiness     = rs.getInt("type_of_business")
+                  )
+                }
+                b.result()
+              } finally closeQuietly(rs)
+            }
+          }
 
-        // Build final response
-        val result = MgdCertificate(
-          mgdRegNumber         = storedProcedure.getString("MGD_REG_NUMBER"),
-          registrationDate     = optDate("REGISTRATION_DATE"),
-          individualName       = optString("INDIVIDUAL_NAME"),
-          businessName         = optString("BUSINESS_NAME"),
-          tradingName          = optString("TRADING_NAME"),
-          repMemName           = optString("REP_MEM_NAME"),
-          busAddrLine1         = optString("BUS_ADDR_LINE1"),
-          busAddrLine2         = optString("BUS_ADDR_LINE2"),
-          busAddrLine3         = optString("BUS_ADDR_LINE3"),
-          busAddrLine4         = optString("BUS_ADDR_LINE4"),
-          busPostcode          = optString("BUS_POSTCODE"),
-          busCountry           = optString("BUS_COUNTRY"),
-          busAdi               = optString("BUS_ADI"),
-          repMemLine1          = optString("REP_MEM_LINE1"),
-          repMemLine2          = optString("REP_MEM_LINE2"),
-          repMemLine3          = optString("REP_MEM_LINE3"),
-          repMemLine4          = optString("REP_MEM_LINE4"),
-          repMemPostcode       = optString("REP_MEM_POSTCODE"),
-          repMemAdi            = optString("REP_MEM_ADI"),
-          typeOfBusiness       = optString("TYPE_OF_BUSINESS"),
-          businessTradeClass   = optInt("BUSINESS_TRADE_CLASS"),
-          noOfPartners         = optInt("NO_OF_PARTNERS"),
-          groupReg             = storedProcedure.getString("GROUP_REG"),
-          noOfGroupMems        = optInt("NO_OF_GROUP_MEMS"),
-          dateCertIssued       = optDate("DATE_CERT_ISSUED"),
-          partMembers          = partMembers,
-          groupMembers         = groupMembers,
-          returnPeriodEndDates = returnPeriods
-        )
+          val groupMembers: List[GroupMember] = {
+            val rs = cs.getObject(27).asInstanceOf[java.sql.ResultSet]
+            if (rs == null) Nil
+            else {
+              try {
+                val b = List.newBuilder[GroupMember]
+                while (rs.next()) {
+                  b += GroupMember(rs.getString("names_of_group_mems"))
+                }
+                b.result()
+              } finally closeQuietly(rs)
+            }
+          }
 
-        storedProcedure.close()
-        connection.close()
+          val returnPeriods: List[ReturnPeriodEndDate] = {
+            val rs = cs.getObject(29).asInstanceOf[java.sql.ResultSet]
+            if (rs == null) Nil
+            else {
+              try {
+                val b = List.newBuilder[ReturnPeriodEndDate]
+                while (rs.next()) {
+                  val d = rs.getDate("return_period_end_date")
+                  if (d != null) b += ReturnPeriodEndDate(d.toLocalDate)
+                }
+                b.result()
+              } finally closeQuietly(rs)
+            }
+          }
 
-        result
+          MgdCertificate(
+            mgdRegNumber         = cs.getString(2),
+            registrationDate     = optDate(3),
+            individualName       = optString(4),
+            businessName         = optString(5),
+            tradingName          = optString(6),
+            repMemName           = optString(7),
+            busAddrLine1         = optString(8),
+            busAddrLine2         = optString(9),
+            busAddrLine3         = optString(10),
+            busAddrLine4         = optString(11),
+            busPostcode          = optString(12),
+            busCountry           = optString(13),
+            busAdi               = optString(14),
+            repMemLine1          = optString(15),
+            repMemLine2          = optString(16),
+            repMemLine3          = optString(17),
+            repMemLine4          = optString(18),
+            repMemPostcode       = optString(19),
+            repMemAdi            = optString(20),
+            typeOfBusiness       = optString(21),
+            businessTradeClass   = optInt(22),
+            noOfPartners         = optInt(23),
+            groupReg             = cs.getString(25),
+            noOfGroupMems        = optInt(26),
+            dateCertIssued       = optDate(28),
+            partMembers          = partMembers,
+            groupMembers         = groupMembers,
+            returnPeriodEndDates = returnPeriods
+          )
+
+        } finally {
+          closeQuietly(cs)
+        }
       }
-    }
+    })(ec)
   }
 
   override def getReturnSummary(mgdRegNumber: String): Future[ReturnSummary] = {
