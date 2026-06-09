3c416628fae77a18c45adfc62b164ce3b079d30e
RajanCP <176382469+RajanCP@users.noreply.github.com>
2026-06-09 16:57:59 +0100

    Initial commit - model, controller, repo, service
    # Conflicts:
    #	app/uk/gov/hmrc/rdsdatacacheproxy/Module.scala
    #	conf/app.routes
    
    # Conflicts:
    #	app/uk/gov/hmrc/rdsdatacacheproxy/Module.scala
    #	conf/app.routes

---
+++ app/uk/gov/hmrc/rdsdatacacheproxy/gambling/repositories/InterestAccruingDetailsDataCacheRepository.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2026 HM Revenue &amp; Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.gambling.repositories
+
+import play.api.Logging
+import play.api.db.NamedDatabase
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.{InterestAccruingDetailsItem, InterestAccruingDetails, Regime}
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.repositories.RepositorySupport.{GTRDatabase, MGDDatabase}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+trait InterestAccruingDetailsDataSource {
+  def getInterestAccruingDetails(regime: Regime, regNumber: String, paginationStart: Int, paginationMaxRows: Int): Future[InterestAccruingDetails]
+}
+
+@Singleton
+class InterestAccruingDetailsDataCacheRepository @Inject() (
+                                                             @NamedDatabase("gambling") mgdDb: MGDDatabase,
+                                                             @NamedDatabase("gambling.gtr") gtrDb: GTRDatabase
+                                                           )(implicit ec: ExecutionContext)
+  extends InterestAccruingDetailsDataSource
+    with RepositorySupport
+    with Logging {
+
+  override def getInterestAccruingDetails(regime: Regime, regNumber: String, paginationStart: Int, paginationMaxRows: Int): Future[InterestAccruingDetails] =
+    Future {
+      getDb(regime, mgdDb, gtrDb).underlying.withConnection { connection =>
+        val cs =
+          regime match
+            case Regime.MGD => connection.prepareCall("{ call MGD_LNP_PK.getMGDInterestAccruingDetails(?, ?, ?, ?, ?, ?, ?, ?) }")
+            case _          => connection.prepareCall("{ call GTR_LNP_PK.getGTRInterestAccruingDetails(?, ?, ?, ?, ?, ?, ?, ?) }")
+
+        try {
+          cs.setString(1, regNumber) // IN  P_REG_NUMBER
+          cs.setInt(2, paginationStart) // IN  P_PAGINATION_START
+          cs.setInt(3, paginationMaxRows) // IN  P_PAGINATION_MAX_ROWS
+          cs.registerOutParameter(4, java.sql.Types.DATE) // OUT P_GTR_PERIOD_START_DATE
+          cs.registerOutParameter(5, java.sql.Types.DATE) // OUT P_GTR_PERIOD_END_DATE
+          cs.registerOutParameter(6, java.sql.Types.DECIMAL) // OUT P_TOTAL (NUMBER)
+          cs.registerOutParameter(7, java.sql.Types.NUMERIC) // OUT P_TOTAL_RECORDS (NUMBER)
+          cs.registerOutParameter(8, oracle.jdbc.OracleTypes.CURSOR) // OUT C_INTEREST_ACCRUING_DETAILS (REF CURSOR)
+          cs.execute()
+
+          val interestAccruingDetails: List[InterestAccruingDetailsItem] = {
+            val rs = cs.getObject(8).asInstanceOf[java.sql.ResultSet]
+            if (rs == null) Nil
+            else {
+              try {
+                val b = List.newBuilder[InterestAccruingDetailsItem]
+                while (rs.next()) {
+                  b += InterestAccruingDetailsItem(
+                    descriptionCode  = Option(rs.getInt("p_desc_code")),
+                    amount           = optDecimalFromLabel("p_amount", rs),
+                    interestID       = Option(rs.getString("p_interest_id")),
+                    periodStartDate  = Option(rs.getDate("p_period_start")).map(_.toLocalDate),
+                    periodEndDate    = Option(rs.getDate("p_period_end")).map(_.toLocalDate)
+                  )
+                }
+                b.result()
+              } finally closeQuietly(rs)
+            }
+          }
+
+          InterestAccruingDetails(
+            periodStartDate = optDate(4, cs),
+            periodEndDate   = optDate(5, cs),
+            total           = optDecimalFromIndex(6, cs),
+            totalRecords    = optInt(7, cs),
+            items           = interestAccruingDetails
+          )
+
+        } finally {
+          closeQuietly(cs)
+        }
+      }
+    }(ec)
+}
---
+++ app/uk/gov/hmrc/rdsdatacacheproxy/gambling/services/InterestAccruingDetailsService.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 HM Revenue &amp; Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.gambling.services
+
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.errors.StatementError
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.{InterestAccruingDetails, Regime}
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.repositories.InterestAccruingDetailsDataSource
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class InterestAccruingDetailsService @Inject()(
+                                                repository: InterestAccruingDetailsDataSource
+                                              )(implicit ec: ExecutionContext)
+  extends BaseService {
+
+  def getInterestAccruingDetails(regime: String, rawRegNumber: String, paginationStart: Int, paginationMaxRows: Int)(implicit
+                                                                                                                     hc: HeaderCarrier
+  ): Future[Either[StatementError, InterestAccruingDetails]] = {
+    withValidParams(regime, rawRegNumber.trim.toUpperCase, paginationStart, paginationMaxRows, "getInterestAccruingDetails")(
+      repository.getInterestAccruingDetails
+    )
+  }
+}
