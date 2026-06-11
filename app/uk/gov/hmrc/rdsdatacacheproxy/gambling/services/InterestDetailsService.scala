2e51e30d97b2872461387f271ee442518b4242b2
narendrapaduchuri <115544611+narendrapaduchuri@users.noreply.github.com>
2026-06-11 09:53:30 +0100

    DTR-5478 GTR-InterestDetails (#121)
    * DTR-5478 GTR-InterestDetails

---
+++ app/uk/gov/hmrc/rdsdatacacheproxy/gambling/services/InterestDetailsService.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.gambling.services
+
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.errors.StatementError
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.models.{InterestDetails, Regime}
+import uk.gov.hmrc.rdsdatacacheproxy.gambling.repositories.InterestDetailsDataSource
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class InterestDetailsService @Inject() (
+  repository: InterestDetailsDataSource
+)(implicit ec: ExecutionContext)
+    extends BaseService {
+
+  def getInterestDetails(regime: String, rawRegNumber: String, paginationStart: Int, paginationMaxRows: Int)(implicit
+    hc: HeaderCarrier
+  ): Future[Either[StatementError, InterestDetails]] =
+    withValidParams(regime, rawRegNumber.trim.toUpperCase, paginationStart, paginationMaxRows, "getInterestDetails")(
+      repository.getInterestDetails
+    )
+}
