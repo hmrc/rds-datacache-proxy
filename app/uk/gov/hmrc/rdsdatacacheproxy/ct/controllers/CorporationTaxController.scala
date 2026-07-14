2a9d9dfa72e320f4dbb867b6399e2a4f9346cdfb
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-14 09:12:00 +0100

    interest accural list
    # Conflicts:
    #	conf/app.routes

---
+++ app/uk/gov/hmrc/rdsdatacacheproxy/ct/controllers/CorporationTaxController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.ct.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.rdsdatacacheproxy.ct.models.{InterestAccuralList, Penalties}
+import uk.gov.hmrc.rdsdatacacheproxy.actions.AuthAction
+import uk.gov.hmrc.rdsdatacacheproxy.ct.repositories.CorporationTaxDatacacheRepository
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class CorporationTaxController @Inject() (
+  authorise: AuthAction,
+  ctCoreDatacacheRepository: CorporationTaxDatacacheRepository,
+  cc: ControllerComponents
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc)
+    with Logging {
+
+  def getPenaltyTransactionList(taxRef: Long, accPeriod: Long): Action[AnyContent] = authorise.async { implicit request =>
+    ctCoreDatacacheRepository
+      .getPenaltyTransactionList(taxRef, accPeriod)
+      .map { penaltyTransactions =>
+        Ok(Json.toJson(Penalties(penaltyTransactions = penaltyTransactions)))
+      }
+      .recover { case ex: Exception =>
+        logger.error("Error while retrieving penalties", ex)
+        InternalServerError(Json.obj("error" -> "Failed to retrieve penalties"))
+      }
+  }
+
+  def getInterestAccuralList(taxRef: Long, accPeriod: Long, interestType: String): Action[AnyContent] = authorise.async { implicit request =>
+    ctCoreDatacacheRepository
+      .getInterestAccuralList(taxRef, accPeriod, interestType)
+      .map { interestAccuralLists =>
+        Ok(Json.toJson(InterestAccuralList(interestAccuralLists)))
+      }
+      .recover { case ex: Exception =>
+        logger.error("Error while retrieving interest accural list", ex)
+        InternalServerError(Json.obj("error" -> "Failed to retrieve  interest accural list"))
+      }
+  }
+
+}
---
+++ app/uk/gov/hmrc/rdsdatacacheproxy/ct/repositories/CorporationTaxDatacacheRepository.scala
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.ct.repositories
+
+import com.google.inject.ImplementedBy
+import oracle.jdbc.OracleTypes
+import play.api.Logging
+import play.api.db.{Database, NamedDatabase}
+import uk.gov.hmrc.rdsdatacacheproxy.ct.models.{InterestAccural, PenaltyTransaction}
+
+import java.sql.*
+import javax.inject.Inject
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[CorporationTaxDatacacheRepositoryImpl])
+trait CorporationTaxDatacacheRepository {
+  def getPenaltyTransactionList(taxRef: Long, accPeriod: Long): Future[List[PenaltyTransaction]]
+  def getInterestAccuralList(taxRef: Long, accPeriod: Long, interestType: String): Future[List[InterestAccural]]
+}
+
+class CorporationTaxDatacacheRepositoryImpl @Inject() (
+  @NamedDatabase("ct-core") db: Database
+)(implicit ec: ExecutionContext)
+    extends CorporationTaxDatacacheRepository
+    with Logging {
+
+  def getPenaltyTransactionList(taxRef: Long, accPeriod: Long): Future[List[PenaltyTransaction]] = {
+    logger.info(s"Input request: taxRef, accPeriod: <$taxRef>, <$accPeriod>")
+    Future {
+      db.withConnection { connection =>
+        val storedProcedure = connection.prepareCall("{call CT_DC_PK.getPenaltyTransactionList(?, ?, ?)}")
+
+        storedProcedure.setLong(1, taxRef)
+        storedProcedure.setLong(2, accPeriod)
+        storedProcedure.registerOutParameter(3, OracleTypes.CURSOR) // p_list
+
+        storedProcedure.execute()
+
+        val pListRs = storedProcedure.getObject(3, classOf[ResultSet])
+
+        try {
+          val penalties = Option(pListRs).map(readPenaltiesTransaction).getOrElse(List.empty)
+          penalties
+        } finally {
+          storedProcedure.close()
+        }
+      }
+    }
+  }
+
+  private def readPenaltiesTransaction(rs: ResultSet): List[PenaltyTransaction] = {
+    val buffer = ListBuffer[PenaltyTransaction]()
+    while (rs.next()) {
+      buffer += PenaltyTransaction(
+        penaltyDate   = Option(rs.getDate("penalty_date")).map(_.toLocalDate).get,
+        `type`        = rs.getString("type"),
+        postingAmount = rs.getBigDecimal("posting_amount")
+      )
+    }
+    buffer.toList
+  }
+
+  def getInterestAccuralList(taxRef: Long, accPeriod: Long, interestType: String): Future[List[InterestAccural]] = {
+    Future {
+      db.withConnection { connection =>
+        val storedProcedure = connection.prepareCall("{call CT_DC_PK.getInterestAccrualList(?, ?, ?, ?)}")
+
+        storedProcedure.setLong(1, taxRef)
+        storedProcedure.setLong(2, accPeriod)
+        storedProcedure.setString(3, interestType)
+        storedProcedure.registerOutParameter(4, OracleTypes.CURSOR)
+
+        storedProcedure.execute()
+
+        val results = storedProcedure.getObject(3, classOf[ResultSet])
+
+        try {
+          val interestAccuralLists = Option(results).map(readInterestAccuralListTransaction).getOrElse(List.empty)
+          interestAccuralLists
+        } finally {
+          storedProcedure.close()
+        }
+      }
+    }
+  }
+
+  private def readInterestAccuralListTransaction(rs: ResultSet): List[InterestAccural] = {
+    val buffer = ListBuffer[InterestAccural]()
+    while (rs.next()) {
+      buffer += InterestAccural(
+        computationAmount       = Option(rs.getBigDecimal("computation_amount")).get,
+        interestAccrualFromDate = Option(rs.getDate("interest_accrual_from_date")).map(_.toLocalDate).get,
+        interestAccrualToDate   = Option(rs.getDate("interest_accrual_from_date")).map(_.toLocalDate).get,
+        interestRate            = Option(rs.getBigDecimal("interest_rate")).get,
+        interestAmount          = Option(rs.getBigDecimal("interest_amount")).get,
+        apEndDate               = Option(rs.getDate("ap_end_date")).map(_.toLocalDate).get
+      )
+    }
+    buffer.toList
+  }
+
+}
