7f6fd1be14946c54062f45c020c6d7f129191172
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-29 12:49:25 +0100

    DTR-6382: Interest Accrual List functionality
    # Conflicts:
    #	conf/app.routes

---
+++ it/test/uk/gov/hmrc/rdsdatacacheproxy/ct/controllers/InterestAccrualListISpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.ct.controllers
+
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK, UNAUTHORIZED}
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.{Application, inject}
+import uk.gov.hmrc.rdsdatacacheproxy.ct.models.{InterestAccrual, InterestAccrualList}
+import uk.gov.hmrc.rdsdatacacheproxy.ct.repositories.InterestAccrualListDatacacheRepository
+import uk.gov.hmrc.rdsdatacacheproxy.ct.stub.InterestAccrualListStubData
+import uk.gov.hmrc.rdsdatacacheproxy.itutil.{ApplicationWithWiremock, AuthStub}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class InterestAccrualListISpec extends AnyWordSpec with Matchers with ScalaFutures with IntegrationPatience with ApplicationWithWiremock {
+
+  class InterestAccrualListStub extends InterestAccrualListDatacacheRepository {
+    override def getInterestAccrualList(taxRef: Long, accPeriod: Long, interestType: String): Future[List[InterestAccrual]] = {
+      Future.successful(InterestAccrualListStubData.getAccrualInterestListItems(taxRef, accPeriod, interestType))
+    }
+  }
+
+  override lazy val app: Application =
+    new GuiceApplicationBuilder()
+      .configure(extraConfig)
+      .overrides(
+        bind[InterestAccrualListDatacacheRepository].toInstance(new InterestAccrualListStub)
+      )
+      .build()
+
+  private val endpoint = "/corporation-tax"
+  private final val taxRef1: Long = 1
+  private final val accPeriod1: Long = 1
+  private final val interestType: String = "IDB";
+
+  "GET /corporation-tax/interest-accrual-list" should {
+
+    "return 200 with InterestAccrual list contains two items" in {
+      AuthStub.authorised()
+
+      val response = get(s"$endpoint/interest-accrual-list/1/2/IDE").futureValue
+
+      response.status mustBe OK
+      response.contentType mustBe "application/json"
+
+      println("Debug JSON:")
+      println(response.json)
+
+      response.json.as[List[InterestAccrual]] mustBe InterestAccrualListStubData.getAccrualInterestListItems(taxRef1, accPeriod1, interestType)
+    }
+
+    "return 200 with InterestAccrual empty list" in {
+
+      AuthStub.authorised()
+
+      val response = get(s"$endpoint/interest-accrual-list/19/2/IDE").futureValue
+
+      response.status mustBe OK
+      response.contentType mustBe "application/json"
+
+      println("Debug JSON:")
+      println(response.json);
+
+      response.json.as[List[InterestAccrual]] mustBe InterestAccrualListStubData.getAccrualInterestListItems(19L, accPeriod1, interestType)
+    }
+
+    "return 500 when stub simulates failure" in {
+
+      AuthStub.authorised()
+
+      val response = get(s"$endpoint/interest-accrual-list/99/99/NON").futureValue
+
+      response.status mustBe INTERNAL_SERVER_ERROR
+
+    }
+
+    "return 401 when unauthorised" in {
+
+      AuthStub.unauthorised()
+
+      val response = get(s"$endpoint/interest-accrual-list/1/2/IDE").futureValue
+
+      response.status mustBe UNAUTHORIZED
+    }
+  }
+
+}
---
+++ test/uk/gov/hmrc/rdsdatacacheproxy/ct/controllers/InterestAccrualListControllerSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rdsdatacacheproxy.ct.controllers
+
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.*
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.should.Matchers.{should, shouldBe}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
+import play.api.test.Helpers.*
+import uk.gov.hmrc.rdsdatacacheproxy.base.SpecBase
+import uk.gov.hmrc.rdsdatacacheproxy.ct.models.InterestAccrual
+import uk.gov.hmrc.rdsdatacacheproxy.ct.services.InterestAccrualService
+
+import scala.concurrent.Future
+
+class InterestAccrualListControllerSpec extends SpecBase with MockitoSugar {
+
+  private class SetUp {
+    val mockService: InterestAccrualService = mock[InterestAccrualService]
+    val controller: InterestAccrualListController = new InterestAccrualListController(fakeAuthAction, mockService, cc)
+
+    val emptyInterestAccrualList: List[InterestAccrual] = List[InterestAccrual]()
+
+  }
+
+  "CorporationTaxController#getInterestAccrualList" - {
+    val taxRef: Long = 17L
+    val accPeriod: Long = 2L
+    val interestType: String = "IDE"
+
+    "return 200 and a successful response when repository return empty interest accrual list " in new SetUp {
+      when(mockService.getInterestAccrualList(any[Long], any[Long], any[String]))
+        .thenReturn(Future.successful(emptyInterestAccrualList))
+
+      val result: Future[Result] = controller.getInterestAccrualList(taxRef, accPeriod, interestType)(fakeRequest)
+
+      status(result)      shouldBe OK
+      contentType(result) shouldBe Some("application/json")
+      verify(mockService).getInterestAccrualList(taxRef, accPeriod, interestType)
+    }
+
+  }
+
+}
