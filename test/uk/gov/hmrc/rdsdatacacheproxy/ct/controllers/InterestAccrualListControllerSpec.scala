9b8e08ebff8ecc83956818fb30bc8e5e2252a11a
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-30 11:57:32 +0100

    DTR-6382: Interest Accrual List added missing tests

--- test/uk/gov/hmrc/rdsdatacacheproxy/ct/controllers/InterestAccrualListControllerSpec.scala
+++ test/uk/gov/hmrc/rdsdatacacheproxy/ct/controllers/InterestAccrualListControllerSpec.scala
@@ -28,6 +28,7 @@ import uk.gov.hmrc.rdsdatacacheproxy.base.SpecBase
 import uk.gov.hmrc.rdsdatacacheproxy.ct.models.InterestAccrual
 import uk.gov.hmrc.rdsdatacacheproxy.ct.services.InterestAccrualService
 
+import java.time.LocalDate
 import scala.concurrent.Future
 
 class InterestAccrualListControllerSpec extends SpecBase with MockitoSugar {
@@ -38,22 +39,71 @@ class InterestAccrualListControllerSpec extends SpecBase with MockitoSugar {
 
     val emptyInterestAccrualList: List[InterestAccrual] = List[InterestAccrual]()
 
+    val interestAccrualListSingleItem: List[InterestAccrual] = List(
+      InterestAccrual(
+        computationAmount       = 1,
+        interestAccrualFromDate = LocalDate.of(2021, 3, 7),
+        interestAccrualToDate   = LocalDate.of(2021, 5, 7),
+        interestRate            = 2,
+        interestAmount          = 10,
+        apEndDate               = LocalDate.of(2021, 6, 7)
+      )
+    )
+
+    val interestAccrualListMultipleItems: List[InterestAccrual] = List(
+      InterestAccrual(
+        computationAmount       = 1,
+        interestAccrualFromDate = LocalDate.of(2021, 3, 7),
+        interestAccrualToDate   = LocalDate.of(2021, 5, 7),
+        interestRate            = 2,
+        interestAmount          = 10,
+        apEndDate               = LocalDate.of(2021, 6, 7)
+      ),
+      InterestAccrual(
+        computationAmount       = 1,
+        interestAccrualFromDate = LocalDate.of(2021, 6, 10),
+        interestAccrualToDate   = LocalDate.of(2021, 8, 10),
+        interestRate            = 3,
+        interestAmount          = 23,
+        apEndDate               = LocalDate.of(2021, 9, 10)
+      )
+    )
+
   }
 
   "InterestAccrualListControllerSpec" - {
-    val taxRef: Long = 17L
-    val accPeriod: Long = 2L
-    val interestType: String = "IDE"
+
+    "return 200 and a successful response when repository return a single item" in new SetUp {
+      when(mockService.getInterestAccrualList(any[Long], any[Long], any[String]))
+        .thenReturn(Future.successful(interestAccrualListSingleItem))
+
+      val result: Future[Result] = controller.getInterestAccrualList(1L, 1L, "IDB")(fakeRequest)
+
+      status(result)      shouldBe OK
+      contentType(result) shouldBe Some("application/json")
+      verify(mockService).getInterestAccrualList(1L, 1L, "IDB")
+    }
+
+    "return 200 and a successful response when repository return a multiple items" in new SetUp {
+      when(mockService.getInterestAccrualList(any[Long], any[Long], any[String]))
+        .thenReturn(Future.successful(interestAccrualListMultipleItems))
+
+      val result: Future[Result] = controller.getInterestAccrualList(2L, 1L, "IDB")(fakeRequest)
+
+      status(result)      shouldBe OK
+      contentType(result) shouldBe Some("application/json")
+      verify(mockService).getInterestAccrualList(2L, 1L, "IDB")
+    }
 
     "return 200 and a successful response when repository return empty interest accrual list " in new SetUp {
       when(mockService.getInterestAccrualList(any[Long], any[Long], any[String]))
         .thenReturn(Future.successful(emptyInterestAccrualList))
 
-      val result: Future[Result] = controller.getInterestAccrualList(taxRef, accPeriod, interestType)(fakeRequest)
+      val result: Future[Result] = controller.getInterestAccrualList(17L, 1L, "IDB")(fakeRequest)
 
       status(result)      shouldBe OK
       contentType(result) shouldBe Some("application/json")
-      verify(mockService).getInterestAccrualList(taxRef, accPeriod, interestType)
+      verify(mockService).getInterestAccrualList(17L, 1L, "IDB")
     }
 
     "return 500 and when repository call fails" in new SetUp {
