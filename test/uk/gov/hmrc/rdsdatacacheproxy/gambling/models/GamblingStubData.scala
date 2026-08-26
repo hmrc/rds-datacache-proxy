3bd9e4723ab0a621a1f487e4ddc75e88f3d6d85a
Milosz Jakubanis <301772132+milosz-jakubanis@users.noreply.github.com>
2026-08-26 10:49:07 +0100

    fix: Remove PartnerDetailsCache and Service (#164)
    * fix: Remove PartnerDetailsCache and Service
    
    Moved their logic to the GamblingCache and
    GamblingService. Relocated tests
    
    # Conflicts:
    #	it/test/uk/gov/hmrc/rdsdatacacheproxy/gambling/controllers/PartnerDetailsControllerISpec.scala
    #	it/test/uk/gov/hmrc/rdsdatacacheproxy/gambling/repositories/PartnerDetailsCacheRepositoryISpec.scala
    #	it/test/uk/gov/hmrc/rdsdatacacheproxy/gambling/stub/PartnerDetailsStubData.scala
    
    * fix: Fix rebase

--- test/uk/gov/hmrc/rdsdatacacheproxy/gambling/models/GamblingStubData.scala
+++ test/uk/gov/hmrc/rdsdatacacheproxy/gambling/models/GamblingStubData.scala
@@ -445,4 +445,91 @@ object GamblingStubData {
           systemDate        = None
         )
     }
+
+  def getPartnerDetailsData(regNumber: String): PartnerDetails =
+    regNumber match {
+      case "XYM00000000000" =>
+        PartnerDetails(
+          partners = List(
+            Partner(
+              mgdRegNumber           = "XYM00000000000",
+              businessPartnerNumber  = Some("0100049899"),
+              dateOfJoining          = Some(LocalDate.of(2025, 1, 1)),
+              dateOfLeaving          = Some(LocalDate.of(2026, 1, 1)),
+              solePropTitle          = Some("Ms"),
+              solePropFirstName      = Some("Amelia"),
+              solePropMiddleName     = Some("Rose"),
+              solePropLastName       = Some("Hartley"),
+              businessName           = Some("Hartley Financial Services"),
+              tradingName            = Some("Hartley Advisory"),
+              dateOfBirth            = Some(LocalDate.of(1986, 9, 22)),
+              nino                   = Some("QQ123456C"),
+              utr                    = Some("1234567890"),
+              vrn                    = Some("GB123456789"),
+              crn                    = Some("09876543"),
+              dateOfIncorporation    = Some(LocalDate.of(2022, 11, 1)),
+              countryOfIncorporation = Some("United Kingdom"),
+              foreignCorporateRef    = Some("FCR-UK-987654"),
+              address1               = Some("42 Mockingbird Lane"),
+              address2               = Some("Suite 5"),
+              address3               = Some("Westbridge Business Park"),
+              address4               = Some("Bristol"),
+              postcode               = Some("BS1 4AB"),
+              country                = Some("United Kingdom"),
+              adi                    = Some("ADI-123456"),
+              iomOrCiFlag            = Some("N"),
+              phoneNumber            = Some("0117 555 1234"),
+              mobilePhoneNumber      = Some("07700 900123"),
+              faxNumber              = Some("0117 555 5678"),
+              emailAddr              = Some("amelia.hartley@example.test"),
+              isFutureLeaveDate      = Some(1),
+              isFutureJoinDate       = Some(0),
+              businessType           = Some(2)
+            )
+          ),
+          systemDate = Some(LocalDate.of(2026, 7, 30))
+        )
+      case "XYZ00000000001" =>
+        PartnerDetails(
+          partners = List(
+            Partner(
+              mgdRegNumber           = "XYZ00000000001",
+              businessPartnerNumber  = Some("0100049899"),
+              dateOfJoining          = Some(LocalDate.of(2025, 1, 1)),
+              dateOfLeaving          = Some(LocalDate.of(2026, 1, 1)),
+              solePropTitle          = Some("Mr"),
+              solePropFirstName      = Some("Tom"),
+              solePropMiddleName     = Some("Jack"),
+              solePropLastName       = Some("Hartley"),
+              businessName           = Some("Hartley Financial Services"),
+              tradingName            = Some("Hartley Advisory"),
+              dateOfBirth            = Some(LocalDate.of(1986, 9, 22)),
+              nino                   = Some("QQ123456C"),
+              utr                    = Some("1234567890"),
+              vrn                    = Some("GB123456789"),
+              crn                    = Some("09876543"),
+              dateOfIncorporation    = Some(LocalDate.of(2022, 11, 1)),
+              countryOfIncorporation = Some("United Kingdom"),
+              foreignCorporateRef    = Some("FCR-UK-987654"),
+              address1               = Some("42 Mockingbird Lane"),
+              address2               = Some("Suite 5"),
+              address3               = Some("Westbridge Business Park"),
+              address4               = Some("Bristol"),
+              postcode               = Some("BS1 4AB"),
+              country                = Some("United Kingdom"),
+              adi                    = Some("ADI-123456"),
+              iomOrCiFlag            = Some("N"),
+              phoneNumber            = Some("0117 555 1234"),
+              mobilePhoneNumber      = Some("07700 900123"),
+              faxNumber              = Some("0117 555 5678"),
+              emailAddr              = Some("amelia.hartley@example.test"),
+              isFutureLeaveDate      = Some(1),
+              isFutureJoinDate       = Some(0),
+              businessType           = Some(2)
+            )
+          ),
+          systemDate = Some(LocalDate.of(2026, 7, 30))
+        )
+      case "XEM33333333333" => throw new RuntimeException("Simulated downstream failure")
+    }
 }
